cf5bc93720a175761d73309960ad9c2dbad8112b
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-13 22:40:34 +0530

    discord bot integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World!')
+import { Client, IntentsBitField } from 'discord.js';
+import { EventHandler } from './core/discord/eventhandler.js';
+import { ENV } from './config/config.js';
+
+/* Bot Instance */
+const ClientInstance = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent
+    ]
+})
+
+/* Login to Discord Instance */
+await ClientInstance.login(ENV.DISCORD_TOKEN)
+
+/* Handle Events */
+EventHandler(ClientInstance)
