0e2532bd74fbe6afd9f4ca558e81c0e833c64b8f
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-13 22:41:07 +0530

    configuration file

---
+++ src/config/config.ts
@@ -0,0 +1,8 @@
+import 'dotenv/config'
+
+export const ENV = {
+    /* Node Environment */
+    NODE_ENV: process.env.NODE_ENV,
+    /* Discord Bot Token */
+    DISCORD_TOKEN: process.env.DISCORD_TOKEN
+}
