3fb9d147aa6fb9a694304d2f92755e616b256f67
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-16 20:41:20 +0530

    update event emitter

--- src/core/services/emitter.ts
+++ src/core/services/emitter.ts
@@ -2,5 +2,6 @@ import { EventEmitter } from 'tseep';
 
 /* TypeSafe Node EventEmitter */
 export const AppEvents = new EventEmitter<{
-    InstanceReady: () => void;
+    onReady: () => Promise<void>,
+    Ready: () => void
 }>()
