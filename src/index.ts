30b7befffb3a1f857d7ee9e38a48e9d9cb707561
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-16 20:42:33 +0530

    express web server
    express web server ontop discord bot. this gives the app functionality to communicate with outside world

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,52 @@
-import { Client, IntentsBitField } from 'discord.js';
-import { ENV } from './config/config.js';
-import EventHandler from './core/handlers/eventHandler.js';
+import express, { Application } from 'express'
+import { AddressInfo } from 'net'
+import helmet from 'helmet'
+import cors from 'cors'
 
-/* Bot Instance */
-const ClientInstance = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent
-    ]
+const app: Application = express()
+
+import Routes from './routes/base-routes.js'
+import { ExpressResponse } from './core/utils/response.js'
+import { AppEvents } from './core/services/emitter.js'
+import { ENV, PORT } from './config/config.js'
+import { ExpressRequest } from './routes/middlewares/express-validate.js'
+import { DevelopmentLog } from './core/utils/dev.js'
+import { ClientInstance } from './core/client/client.js'
+
+app.use(express.json())
+app.use(cors())
+app.use(helmet())
+
+app.use((req, res, next) => {
+    DevelopmentLog(req.originalUrl)
+    /* Expose Discrod Client to Incomming Requests */
+    req.xClient = ClientInstance
+    next()
 })
 
-/* Login to Discord Instance */
-await ClientInstance.login(ENV.DISCORD_TOKEN)
+app.use(ExpressRequest)
 
-/* Custom Events Handler */
-EventHandler(ClientInstance)
+app.use('/api', Routes)
+
+app.get('/api', (req, res) => {
+    return ExpressResponse(res, true, 200, {
+        status: 'OK',
+        version: ENV.NPM_PACKAGE_VERSION,
+        message: 'server is up and running...'
+    })
+})
+
+app.use('*', (req, res) => {
+    res.sendStatus(403)
+})
+
+/* Emit onReady signal to bot controller */
+AppEvents.emit('onReady')
+
+/* Start express webserver when bot is ready */
+AppEvents.on('Ready', () => {
+    const server = app.listen(ENV.PORT || PORT, () => {
+        const { address, port } = server.address() as AddressInfo
+        DevelopmentLog(`(Discord) API running in http://${ address }:${ port }`, true)
+    })
+})
