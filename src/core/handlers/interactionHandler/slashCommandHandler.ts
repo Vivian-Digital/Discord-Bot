57b9810a137d96811c148010b3919094bd0b7342
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-17 15:18:05 +0530

    slash command handler

---
+++ src/core/handlers/interactionHandler/slashCommandHandler.ts
@@ -0,0 +1,57 @@
+import { Client, CommandInteraction } from 'discord.js';
+import getLocalCommands from '../../utils/getLocalCommands.js';
+import { CONFIG } from '../../../config/config.js';
+
+/* Default Slash Command Handler */
+export default async (Instance: Client, interaction: CommandInteraction) => {
+    /* get local commands */
+    const localCommands = await getLocalCommands()
+
+    try {
+        /* find command */
+        const commandObject = localCommands.find((cmd) => cmd.name === interaction.commandName)
+        /* the command handler not exists */
+        if (!commandObject) { return }
+
+        /* check if the command is devOnly command */
+        if (commandObject.devOnly) {
+            if (!CONFIG.devAccounts.includes(interaction.member?.user.id as string)) {
+                await interaction.reply({
+                    content: 'Only Developers are allowd to use this command!',
+                    ephemeral: true
+                })
+                return 
+            }
+        }
+
+        /* check if the command requires is only available to selected people */
+        if (commandObject.authorized) {
+            if (!CONFIG.authorizedAccounts.includes(interaction.member?.user.id as string)) {
+                await interaction.reply({
+                    content: 'You don\'t have Permission to run this command!',
+                    ephemeral: true
+                })
+                return 
+            }
+        }
+
+        /* check if the user has enough permission to run this command */
+        if (commandObject.permissionsRequired?.length) {
+            for (const permission of commandObject.permissionsRequired) {
+                if (!interaction.memberPermissions?.has(permission)) {
+                    await interaction.reply({
+                        content: 'Not Enough Permissions!',
+                        ephemeral: true
+                    })
+                    return
+                }
+            }
+        }
+
+        /* execute command */
+        await commandObject.callback(Instance, interaction)
+
+    } catch (error) {
+        console.log(`There was an error running this command ${ (error as Error).message }`)
+    }
+}
