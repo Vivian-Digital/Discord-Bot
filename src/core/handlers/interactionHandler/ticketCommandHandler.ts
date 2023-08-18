98d873008d65675e291fbc48f487c82301495195
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-18 10:35:51 +0530

    automatic ticket handeling

--- src/core/handlers/interactionHandler/ticketCommandHandler.ts
+++ src/core/handlers/interactionHandler/ticketCommandHandler.ts
@@ -1,17 +1,94 @@
-import { ButtonInteraction, Client } from 'discord.js';
+import { ButtonInteraction, ChannelType, Client, ColorResolvable, EmbedBuilder, spoiler } from 'discord.js';
 import { TicketButtons } from '../../events/ready/[03]-ticketInteractions.js';
+import { EMBED_DATA, ENV, TICKET } from '../../../config/config.js';
 
 type TCustomID = typeof TicketButtons[number]['id']
 
-export default async (Instance: Client, interaction: ButtonInteraction) => {
+const InteractionEmbedBuilder = (
+    color: ColorResolvable,
+    description: string
+    ) => {
+        return new EmbedBuilder()
+        .setColor(color)
+        .setDescription(description)
+        .setTimestamp()
+        .setFooter({
+            text: 'Netch LK Service Manger',
+            iconURL: EMBED_DATA.ICONS.GREEN_SHIELD
+        })
+}
+
+export default async (Instance: Client<true>, interaction: ButtonInteraction) => {
     /* Switch Between User Choice */
     switch (interaction.customId as TCustomID) {
         /* Automatic Ticket Handler */
         case 'ticket_automatic': {
-            await interaction.reply({
-                content: 'You have Created a Automatic Ticket',
-                ephemeral: true
-            })
+            const Server = await Instance.guilds.fetch(ENV.GUILD_ID)
+            const TicketCategory = await Server.channels.fetch(TICKET.CATEGORY_ID)
+            
+            if (TicketCategory) {
+                const DynamicChannel = await Server.channels.create({
+                    name: interaction.user.username,
+                    type: ChannelType.GuildText,
+                    topic: `${ interaction.user.tag }'s Ticket`,
+                    reason: `Automatic Ticket Creation via ${ Instance.user.tag }`
+                })
+
+                await Promise.all([
+                    interaction.reply({
+                        embeds: [
+                            InteractionEmbedBuilder('Green', 'Success')
+                        ],
+                        ephemeral: true
+                    }),
+                    DynamicChannel.setParent(TicketCategory.id),
+                    DynamicChannel.send({
+                        embeds: [
+                            InteractionEmbedBuilder(
+                                'Green',
+                                `<@${ interaction.user.id }> have Created a Automatic Ticket \`${ spoiler(interaction.user.id) }\``
+                            )
+                        ],
+                    })
+                ])
+
+                /* Set Channel Permissions */
+                await Promise.all([
+                    /* Add Service Manger to this Channel */
+                    DynamicChannel.permissionOverwrites.create(TICKET.MANGER, {
+                        ViewChannel: true
+                    }),
+                    /* Hide Channel from Everyone Else */
+                    DynamicChannel.permissionOverwrites.create(Server.roles.everyone, {
+                        ViewChannel: false
+                    }),
+                    /* Ticket Creators Permissions */
+                    DynamicChannel.permissionOverwrites.create(interaction.user.id, {
+                        /* Allowed */
+                        ViewChannel: true,
+                        SendMessages: true,
+                        AddReactions: true,
+                        ReadMessageHistory: true,
+                        UseApplicationCommands: true,
+                        /* Not Allowed */
+                        AttachFiles: false,
+                        CreatePublicThreads: false,
+                        CreatePrivateThreads: false,
+                        CreateInstantInvite: false,
+                        EmbedLinks: false,
+                        ManageThreads: false,
+                        ManageMessages: false,
+                        ManageRoles: false,
+                        ManageWebhooks: false,
+                        ManageChannels: false,
+                        MentionEveryone: false,
+                        SendVoiceMessages: false,
+                        SendMessagesInThreads: false,
+                        UseExternalEmojis: false,
+                        UseExternalStickers: false,
+                    })
+                ])
+            }
             return
         }
         /* Manual Option */
