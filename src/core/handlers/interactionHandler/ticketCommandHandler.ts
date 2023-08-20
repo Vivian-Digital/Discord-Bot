b8c4eed5cb3fd3cf139d4009dd5e38adf6fb2872
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-08-20 09:40:44 +0530

    tickets handler

--- src/core/handlers/interactionHandler/ticketCommandHandler.ts
+++ src/core/handlers/interactionHandler/ticketCommandHandler.ts
@@ -1,8 +1,15 @@
-import { ButtonInteraction, ChannelType, Client, ColorResolvable, EmbedBuilder, spoiler } from 'discord.js';
+import { ButtonInteraction, ChannelType, Client, ColorResolvable, EmbedBuilder, Guild, TextChannel, User, spoiler } from 'discord.js';
 import { TicketButtons } from '../../events/ready/[03]-ticketInteractions.js';
 import { EMBED_DATA, ENV, TICKET } from '../../../config/config.js';
+import { NewTicket, isNewTicketUser } from '../../controllers/tickets.js';
+import { DevelopmentLog } from '../../utils/dev.js';
 
 type TCustomID = typeof TicketButtons[number]['id']
+
+const InteractionLabels: Record<TCustomID, typeof TicketButtons[number]['label']> = {
+    'ticket_automatic': 'Automatic',
+    'ticket_manual': 'Manual'
+}
 
 const InteractionEmbedBuilder = (
     color: ColorResolvable,
@@ -18,86 +25,125 @@ const InteractionEmbedBuilder = (
         })
 }
 
+const NewDynamicChannel = async (server: Guild, Instance: Client<true>, user: User) => {
+    const ExistingChannel = await isNewTicketUser(user)
+    if (ExistingChannel) {
+        try {
+            const channel = await server.channels.fetch(ExistingChannel.dynamicChannel)
+            if (channel && channel.isTextBased()) {
+                /* Send Existing Channel if Exists */
+                return channel as TextChannel
+            }
+        } catch (error) {
+            /* Someone Deleted the Channel */
+            DevelopmentLog('Channel Deleted Creating a new One!')
+        }
+    }
+    /* Create New Channel */
+    const channel = await server.channels.create({
+        name: user.username,
+        type: ChannelType.GuildText,
+        topic: `${ user.tag }'s Ticket`,
+        reason: `Automatic Ticket Creation via ${ Instance.user.tag }`
+    })
+    return channel
+}
+
 export default async (Instance: Client<true>, interaction: ButtonInteraction) => {
+    /* Get Guild(server) and ticket category */
+    const Guild = await Instance.guilds.fetch(ENV.GUILD_ID)
+    const TicketCategory = await Guild.channels.fetch(TICKET.CATEGORY_ID)
+
+    /* Channel Category Does not Exists */
+    if (!TicketCategory) {
+        return await interaction.reply({
+            embeds: [
+                InteractionEmbedBuilder(
+                    'Red',
+                    `\`ALL TICKETS\` Category Does not Exists \`ID\`:\`${ spoiler(TICKET.CATEGORY_ID) }\``
+                )
+            ],
+            ephemeral: true
+        })
+    }
+
+    /* Interaction Response & Channel Creation */
+    const [DynamicChannel] = await Promise.all([
+        NewDynamicChannel(Guild, Instance, interaction.user),
+        interaction.reply({
+            embeds: [
+                InteractionEmbedBuilder('Green', 'Success')
+            ],
+            ephemeral: true
+        })
+    ])
+
+    /* Handle Dynamic Channel */
+    await Promise.all([
+        /* Send Reply to Interaction Channel */
+        DynamicChannel.setParent(TicketCategory.id),
+        /* Hide Channel from Everyone Else */
+        DynamicChannel.permissionOverwrites.create(Guild.roles.everyone, {
+            ViewChannel: false
+        }),
+        /* Add Service Manger to this Channel */
+        DynamicChannel.permissionOverwrites.create(TICKET.MANGER, {
+            ViewChannel: true
+        }),
+        /* Ticket Creators Permissions */
+        DynamicChannel.permissionOverwrites.create(interaction.user.id, {
+            /* Allowed */
+            ViewChannel: true,
+            SendMessages: true,
+            AddReactions: true,
+            ReadMessageHistory: true,
+            UseApplicationCommands: true,
+            /* Not Allowed */
+            AttachFiles: false,
+            CreatePublicThreads: false,
+            CreatePrivateThreads: false,
+            CreateInstantInvite: false,
+            EmbedLinks: false,
+            ManageThreads: false,
+            ManageMessages: false,
+            ManageRoles: false,
+            ManageWebhooks: false,
+            ManageChannels: false,
+            MentionEveryone: false,
+            SendVoiceMessages: false,
+            SendMessagesInThreads: false,
+            UseExternalEmojis: false,
+            UseExternalStickers: false,
+        }),
+        NewTicket(interaction.user, DynamicChannel.id)
+    ])
+
+    const ResponseString = `<@${ interaction.user.id }> have Created a ${ InteractionLabels[(interaction.customId as TCustomID)] } Ticket`
+
     /* Switch Between User Choice */
     switch (interaction.customId as TCustomID) {
         /* Automatic Ticket Handler */
         case 'ticket_automatic': {
-            const Server = await Instance.guilds.fetch(ENV.GUILD_ID)
-            const TicketCategory = await Server.channels.fetch(TICKET.CATEGORY_ID)
-            
-            if (TicketCategory) {
-                const DynamicChannel = await Server.channels.create({
-                    name: interaction.user.username,
-                    type: ChannelType.GuildText,
-                    topic: `${ interaction.user.tag }'s Ticket`,
-                    reason: `Automatic Ticket Creation via ${ Instance.user.tag }`
-                })
-
-                await Promise.all([
-                    interaction.reply({
-                        embeds: [
-                            InteractionEmbedBuilder('Green', 'Success')
-                        ],
-                        ephemeral: true
-                    }),
-                    DynamicChannel.setParent(TicketCategory.id),
-                    DynamicChannel.send({
-                        embeds: [
-                            InteractionEmbedBuilder(
-                                'Green',
-                                `<@${ interaction.user.id }> have Created a Automatic Ticket \`${ spoiler(interaction.user.id) }\``
-                            )
-                        ],
-                    })
-                ])
-
-                /* Set Channel Permissions */
-                await Promise.all([
-                    /* Add Service Manger to this Channel */
-                    DynamicChannel.permissionOverwrites.create(TICKET.MANGER, {
-                        ViewChannel: true
-                    }),
-                    /* Hide Channel from Everyone Else */
-                    DynamicChannel.permissionOverwrites.create(Server.roles.everyone, {
-                        ViewChannel: false
-                    }),
-                    /* Ticket Creators Permissions */
-                    DynamicChannel.permissionOverwrites.create(interaction.user.id, {
-                        /* Allowed */
-                        ViewChannel: true,
-                        SendMessages: true,
-                        AddReactions: true,
-                        ReadMessageHistory: true,
-                        UseApplicationCommands: true,
-                        /* Not Allowed */
-                        AttachFiles: false,
-                        CreatePublicThreads: false,
-                        CreatePrivateThreads: false,
-                        CreateInstantInvite: false,
-                        EmbedLinks: false,
-                        ManageThreads: false,
-                        ManageMessages: false,
-                        ManageRoles: false,
-                        ManageWebhooks: false,
-                        ManageChannels: false,
-                        MentionEveryone: false,
-                        SendVoiceMessages: false,
-                        SendMessagesInThreads: false,
-                        UseExternalEmojis: false,
-                        UseExternalStickers: false,
-                    })
-                ])
-            }
-            return
+            return await DynamicChannel.send({
+                embeds: [
+                    InteractionEmbedBuilder(
+                        'Green',
+                        ResponseString
+                    )
+                ],
+            })
         }
         /* Manual Option */
         case 'ticket_manual': {
-            await interaction.reply({
-                content: 'You have Created a Manual Ticket',
-                ephemeral: true
+            return await DynamicChannel.send({
+                embeds: [
+                    InteractionEmbedBuilder(
+                        'Green',
+                        `${ ResponseString }. <@&${ TICKET.STAFF_ROLE }> Member will contact you soon.`
+                    )
+                ],
             })
-            return
+            /* Interaction Response */
         }
     }
 }
