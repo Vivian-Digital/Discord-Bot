4ed93e1b524db6e09ce94e43431d978f7df336e3
Danushka-Madushan <Danushkamadushan128@gmail.com>
2023-09-10 12:07:54 +0530

    environment configurations

--- src/@types/modules/env.d.ts
+++ src/@types/modules/env.d.ts
@@ -6,12 +6,17 @@ declare global {
         interface ProcessEnv {
             NODE_ENV: 'production' | 'development',
             PORT: number,
-            PANEL_USERNAME: string,
-            PANEL_PASSWORD: string,
             DISCORD_TOKEN: string,
             GUILD_ID: string,
             CONNECTION_URI: string,
             npm_package_version: string,
+            /* vps credentials */
+            [key: `VPS${ number }_API`]: `https://${ string }.netchlk.org:${ number }`,
+            [key: `VPS${ number }_USERNAME`]: string,
+            [key: `VPS${ number }_PASSWORD`]: string,
+            ZOOM_VPS_API: `https://${ string }.netchlk.org:${ number }`,
+            ZOOM_VPS_USERNAME: string,
+            ZOOM_VPS_PASSWORD: string
         }
     }
 }
